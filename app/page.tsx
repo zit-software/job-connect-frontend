4f7c6c598a3d3b883b81d1241bf48c56731779d6
Kim Minh Thang <thangved02@gmail.com>
2023-10-03 19:31:19 +0700

    chore: add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,32 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import NextLink from 'next/link';
+import { Link } from '@nextui-org/link';
+import { button as buttonStyles } from '@nextui-org/theme';
+import { siteConfig } from '@/config/site';
+import { title, subtitle } from '@/components/primitives';
+import { GithubIcon } from '@/components/icons';
 
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block w-100 text-center justify-center">
-        <h1 className={title({ color: "violet" })}>Connecting </h1>
-        <h1 className={title()}>IT Recruiters and Applicants</h1>
-      </div>
+	return (
+		<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
+			<div className='inline-block w-100 text-center justify-center'>
+				<h1 className={title({ color: 'violet' })}>Connecting </h1>
+				<h1 className={title()}>IT Recruiters and Applicants</h1>
+			</div>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          as={NextLink}
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-    </section>
-  );
+			<div className='flex gap-3'>
+				<Link
+					isExternal
+					as={NextLink}
+					className={buttonStyles({
+						variant: 'bordered',
+						radius: 'full',
+					})}
+					href={siteConfig.links.github}
+				>
+					<GithubIcon size={20} />
+					GitHub
+				</Link>
+			</div>
+		</section>
+	);
 }
--- config/site.ts
+++ config/site.ts
@@ -1,21 +1,22 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [],
-  links: {
-    github: "https://github.com/zit-software",
-    facebook: "https://www.facebook.com/zit.software/",
-  },
+	name: 'Next.js + NextUI',
+	description:
+		'Make beautiful websites regardless of your design experience.',
+	navItems: [
+		{
+			label: 'Home',
+			href: '/',
+		},
+		{
+			label: 'About',
+			href: '/about',
+		},
+	],
+	navMenuItems: [],
+	links: {
+		github: 'https://github.com/zit-software',
+		facebook: 'https://www.facebook.com/zit.software/',
+	},
 };
