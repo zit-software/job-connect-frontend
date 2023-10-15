76665902d5490005c2727c245750dd8d19ac923a
Kim Minh Thang <thangved02@gmail.com>
2023-10-15 14:50:17 +0700

    feat: signin

--- utils/createHttpClient.ts
+++ utils/createHttpClient.ts
@@ -1,9 +1,16 @@
+import tokenService from '@/services/token.service';
 import axios from 'axios';
 
 const createHttpClient = (baseUrl: string = '') => {
 	const client = axios.create({
 		baseURL: `${process.env.NEXT_PUBLIC_API_URL}/${baseUrl}`,
 		withCredentials: true,
+	});
+
+	client.interceptors.request.use((config) => {
+		config.headers.Authorization = `Bearer ${tokenService.accessToken}`;
+
+		return config;
 	});
 
 	client.interceptors.response.use(
