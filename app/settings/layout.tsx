d306e35b8702b188da508a8a3c0c8724fdd027e4
Kim Minh Thang <thangved02@gmail.com>
2023-10-18 10:44:08 +0700

    feat: settings page navigation

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,26 +1,53 @@
 'use client';
 
+import clsx from 'clsx';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavItem = ({ href, children, active = false }: { href: string; children: React.ReactNode; active?: boolean }) => {
+	return (
+		<Link
+			href={href}
+			className={clsx('p-4 rounded-xl flex items-center gap-2 transition-all', {
+				'bg-primary-200 text-primary font-bold': active,
+			})}
+		>
+			{children}
+		</Link>
+	);
+};
+
+const items = [
+	{
+		href: '/settings/profile',
+		icon: 'bx bx-user',
+		text: 'Thông tin cá nhân',
+	},
+	{
+		href: '/settings/2fa',
+		icon: 'bx bx-key',
+		text: 'Xác thực 2 lớp',
+	},
+	{
+		href: '/settings/delete-account',
+		icon: 'bx bx-trash',
+		text: 'Xóa tài khoản',
+	},
+];
 
 export default function SettingsPage({ children }: { children: React.ReactNode }) {
+	const pathname = usePathname();
+
 	return (
 		<div className='w-[1280px] max-w-full mx-auto my-4'>
 			<div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
 				<div className='col-span-1 flex flex-col gap-2'>
-					<Link
-						href='settings/profile'
-						className='p-4 bg-primary text-white font-bold  shadow-blue-400 shadow-lg rounded-xl'
-					>
-						<i className='bx bx-user'></i> <span>Thông tin cá nhân</span>
-					</Link>
-
-					<Link href='settings/2fa' className='p-4 font-bold'>
-						<i className='bx bx-key'></i> <span>Xác thực 2 lớp</span>
-					</Link>
-
-					<Link href='settings/delete-account' className='p-4 font-bold'>
-						<i className='bx bx-trash'></i> <span>Xóa tài khoản</span>
-					</Link>
+					{items.map((item) => (
+						<NavItem key={item.href} href={item.href} active={item.href === pathname}>
+							<i className={item.icon}></i>
+							<span>{item.text}</span>
+						</NavItem>
+					))}
 				</div>
 				<div className='col-span-2 bg-background border rounded-xl py-4 px-8'>{children}</div>
 			</div>
