744b8b0b4b83bb0d65e12198672acb9149171b04
Kim Minh Thang <thangved02@gmail.com>
2023-10-20 19:51:11 +0700

    chore: update page loader

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -1,5 +1,5 @@
 import { Progress } from '@nextui-org/react';
 
 export default function PageLoading() {
-	return <Progress isIndeterminate className='w-full' />;
+	return <Progress isIndeterminate className='w-full' size='sm' />;
 }
