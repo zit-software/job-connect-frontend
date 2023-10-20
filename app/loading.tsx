1c2c51717c29488a745f0954c7bc7ceb16a8609f
TranHuy2k2 <giahuytran2002@gmail.com>
2023-10-20 08:11:47 +0700

    feat: added page loader

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+'use client';
+import PageLoader from '@/components/PageLoader';
+
+export default function Loading() {
+	return <PageLoader />;
+}
---
+++ components/PageLoader.tsx
@@ -0,0 +1,5 @@
+import { Progress } from '@nextui-org/react';
+
+export default function PageLoading() {
+	return <Progress isIndeterminate className='w-full' />;
+}
